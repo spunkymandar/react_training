0831dc0ef62e1b576bee27bafdb2ab52b30e03b2
“Mandar <mandarmjog@gmail.com>
2025-01-23 21:20:36 +0530

    modified todo with the key implementation

--- fourth/src/components/Todos.js
+++ fourth/src/components/Todos.js
@@ -2,21 +2,25 @@ import { useState } from "react"
 
 const Todos=()=>{
 
-    // const todos=['Learn React','Learn Angular']
-    const[todos,setTodos]=useState(['Learn React','Learn Angular'])
+    // const todos=['Learn React','Learn Angular'] : type1
+    //const[todos,setTodos]=useState(['Learn React','Learn Angular']) : type 2
+    const[todos,setTodos]=useState([
+        {todoid:1,todoitem:'Learn React'},
+        {todoid:2,todoitem:'Learn Angular'}
+    ])
     const handleAddTodo=()=>{
         console.log("Button clicked!!")
-       // todos.push('Another course')
         console.log(todos)
-       // setTodos(todos);
-        setTodos(curTodos=>[...curTodos,'Another Course'])
+        //setTodos(curTodos=>[...curTodos,'Another Course'])
+        setTodos(curTodos=>[...curTodos,{todoid:111,todoitem:'Another Item'}])
     }
     return(
         <>
             <p>ToDo's for this week</p>
             <input type="text"></input>
             <button onClick={handleAddTodo}>Add ToDo</button>
-            {todos.map(td=><li >{td}</li>)}
+            {/* {todos.map((td,index)=><li key={index}>{td}</li>)} */}
+            {todos.map((td)=><li key={td.todoid}>{td.todoitem}</li>)}
         </>
     )
 }
